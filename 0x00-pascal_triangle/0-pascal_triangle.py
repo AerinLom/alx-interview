6e3adafaa5c88ebe82eb5b83aa7a3ae40648d9fc
AerinLom <aerinlom1@gmail.com>
2024-07-24 03:51:50 -0700

    Fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,19 @@
 def pascal_triangle(n):
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for level_index in range(1, n):
         current_level = [1]
 
         for element_index in range(1, level_index):
-            current_level.append(triangle[level_index-1][element_index-1] + triangle[level_index-1][element_index])
+            current_level.append(
+                triangle[level_index-1][element_index-1] +
+                triangle[level_index-1][element_index]
+            )
 
         current_level.append(1)
         triangle.append(current_level)
-    
+
     return triangle
