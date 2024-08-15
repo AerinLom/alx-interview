4cce18d4b0cea7ca74165716c681f75fd3547bde
AerinLom <aerinlom1@gmail.com>
2024-08-15 05:09:12 -0700

    Log parsing

--- 0-stats.py
+++ 0-stats.py
@@ -1,47 +1,50 @@
 #!/usr/bin/python3
 """
-A script that reads stdin line by line and computes metrics
+A script that reads stdin line by line and computes metrics.
 """
 import sys
 
 
-def print_stats(total_size, status_codes):
+def print_metrics(status_code_counts, total_file_size):
     """
-    Prints the computed metrics.
+    Method to print the metrics.
     """
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print(f"{code}: {status_codes[code]}")
+    print("File size: {}".format(total_file_size))
+    for status_code, count in sorted(status_code_counts.items()):
+        if count != 0:
+            print("{}: {}".format(status_code, count))
 
 
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_file_size = 0
 line_count = 0
+status_code_counts = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
 
 try:
     for line in sys.stdin:
-        parts = line.split()
+        reversed_line_parts = line.split()[::-1]
 
-        if len(parts) > 6:
-            try:
-                status_code = int(parts[-2])
-                file_size = int(parts[-1])
+        if len(reversed_line_parts) > 2:
+            line_count += 1
 
-                total_size += file_size
-                if status_code in status_codes:
-                    status_codes[status_code] += 1
+            total_file_size += int(reversed_line_parts[0])
+            status_code = reversed_line_parts[1]
 
-                line_count += 1
+            if status_code in status_code_counts:
+                status_code_counts[status_code] += 1
 
-                if line_count % 10 == 0:
-                    print_stats(total_size, status_codes)
+            if line_count == 10:
+                print_metrics(status_code_counts, total_file_size)
+                line_count = 0
 
-            except (ValueError, IndexError):
-                continue
-
-except KeyboardInterrupt:
-    print_stats(total_size, status_codes)
-    raise
-
-print_stats(total_size, status_codes)
+finally:
+    print_metrics(status_code_counts, total_file_size)
